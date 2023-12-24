a4abfcad5e964acdef59b79f614fdd9aa4f597e3
ch33n9 <zzp232323@live.com>
2023-12-24 23:13:57 +0800

    ADD frq2019 q4

---
+++ frq2019/Q4/LightBoard.java
@@ -0,0 +1,14 @@
+package frq2019.Q4;
+
+public class LightBoard {
+    private boolean[][] lights;
+
+    public LightBoard(int numRows, int numCols){
+        //TODO: to be implemented in part(a)
+    }
+
+    public boolean evalluateLight(int row, int col){
+        //TODO: to be implemented in part(b)
+        return false;
+    }
+}
